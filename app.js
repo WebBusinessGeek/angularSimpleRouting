885d21158b52847a56413fdb2c4fce9c88e5b840
Kevin Washington <kevw12188@gmail.com>
2014-09-29 16:57:32 -0400

    quick exmaple setup

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-var app = angular.module('app', [])
+var app = angular.module('app', ['ngRoute']);
 
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/',
+		{
+			template: 'This is the template'
+		})
+		.when('/check',
+		{
+			template: 'This is another template'
+		})
+});
